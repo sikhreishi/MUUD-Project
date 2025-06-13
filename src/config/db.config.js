f0aecf42727cb1068c25dc67c607f8a4f6e31ba7
Nour Ikhreishi <nourikhreishi@gmail.com>
2025-06-13 15:04:36 -0400

    changed db creds

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,11 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = {
